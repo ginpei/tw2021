cbdebe342a0c7670d2721299100b9f2a1b1a6715
ginpei <ginpei@gmail.com>
2021-07-02 07:39:01 +0000

    docs: ✏️ comments

--- src/data/message.ts
+++ src/data/message.ts
@@ -1,5 +1,9 @@
 import { createDataRecord, DataRecord } from "../gp/data/DataRecord";
 
+/**
+ * Core data of message.
+ * Mostly you would use `MessageResolved` instead.
+ */
 export interface Message extends DataRecord {
   body: string;
   userId: string;
--- src/data/messageResolved.ts
+++ src/data/messageResolved.ts
@@ -1,6 +1,10 @@
 import { Message } from "./message";
 import { User } from "./user";
 
+/**
+ * `Message` with `User` data.
+ * Use this instead of `Message` directly.
+ */
 export interface MessageResolved extends Message {
   user: User | null;
 }
