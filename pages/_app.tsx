ef2a14d1b6aedd3178d1d3b5e870bf8a464f6fec
ginpei <ginpei@gmail.com>
2021-07-04 03:32:47 +0000

    feat: 🎸 revert app base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import type { AppProps } from "next/app";
+import { LoginUserProvider } from "../src/data/LoginUserContext";
+import "../src/gp/styles/index.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <LoginUserProvider>
+      <Component {...pageProps} />
+    </LoginUserProvider>
+  );
 }
